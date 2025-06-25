490015d9d5e9840c3266107eb0fee725faf34fa8
Romain Guimbal <39882829+RomainGuimbal@users.noreply.github.com>
2025-06-26 00:33:30 +0200

    update info

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# StepperAddonClone
-A fork of the STEPer add-on which is unfortunately no longer maintained by its original creator
+# STEPper Reborn
+A fork of the STEPper add-on which is no longer maintained by its original creator Ambi.
+Based on OCP instead of OCC, this make it lighter, simpler and cross platform !
--- __init__.py
+++ __init__.py
@@ -14,9 +14,9 @@
 # Copyright 2021 Tommi Hyppänen
 
 bl_info = {
-    "name": "STEPper",
-    "author": "ambi",
-    "description": "STEP OpenCASCADE import",
+    "name": "STEPper Reborn",
+    "author": "Romain Guimbal (maintainer), Ambi (original creator)",
+    "description": "STEP importer, OpenCASCADE-based",
     "blender": (4, 2, 0),
     "version": (2, 0, 0),
     "location": "3D View > Tools panel > Stepper",
